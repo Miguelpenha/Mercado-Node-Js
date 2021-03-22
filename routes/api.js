d42f353e5a555dc8567fbfb12e58a051395b49cc
Miguelpenha <miguel.penha1@gmail.com>
2021-03-22 17:55:39 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,12 @@ const express = require('express')
 const api = express.Router()
 
 api.get('/', (req, res) => {
-    if (req.body.login === process.env.LOGIN_ADMIN && req.body.senha === process.env.SENHA_ADMIN) {
-        ProdutoModels.find({}, (err, result) => {
-            if (result.length === 0) {
-                res.json('Não Existem Produtos Cadastrados')
-            } else {
-                res.json({TotalDeProdutos: result.length, Produtos: result})
-            }
-        })
-    } else {
-        res.json('Senha Ou Login Inválidos')
-    }
+    ProdutoModels.find({}, (err, result) => {
+        if (result.length === 0) {
+            res.json('Não Existem Produtos Cadastrados')
+        } else {
+            res.json({TotalDeProdutos: result.length, Produtos: result})
+        }
+    })
 })
 module.exports = api
